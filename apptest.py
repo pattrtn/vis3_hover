ba879c72bd58be885f59e39c9053a5b25e2322b6
Pattraporn T. <146692261+pattrtn@users.noreply.github.com>
2024-11-17 01:47:52 +0700

    Update apptest.py

--- apptest.py
+++ apptest.py
@@ -46,7 +46,7 @@ def get_color_province(percentage):
     normalized_percentage = np.clip(percentage / 100, 0, 1)
 
     # Use matplotlib's 'Reds' colormap (from white to red)
-    cmap = plt.get_cmap("RdGy")
+    cmap = plt.get_cmap("seismic")
     rgba_color = cmap(normalized_percentage)
     
     # Convert the RGBA color to hex
